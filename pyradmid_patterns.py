ad39c817172f39f97e4ea25e55cdf5f35f2d92b8
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-08-23 23:31:37 +0530

    Patterns Questions

--- pyradmid_patterns.py
+++ pyradmid_patterns.py
@@ -161,3 +161,54 @@ for i in range(n):
 
 print()
 
+           #
+
+n = 5
+for i in range(n - 1):
+    for j in range(i):
+        print(" ", end = "")
+    for k in range(n - i):
+       if k == 0 or k == n - i - 1:
+           print("* ", end = "")
+       else:
+           print("  ", end = "")
+    print()
+
+for i in range(n):
+    for j in range(n - i - 1):
+        print(" ", end = "")
+    for k in range(i+1):
+        if k == 0 or k == i:
+            print("* ", end = "")
+        else:
+            print("  ", end = "")
+    print()
+
+print()
+             #
+
+n = 5
+for i in range(n  ):
+    for j in range(i + 1):
+        if j == 0 or j == i or i == n - 1:
+            print("* ", end = "")
+        else:
+            print("  ", end = "")
+    print()
+
+print()
+
+            #
+
+n = 5
+for i in range(n):
+    for j in range(i):
+        print("  ", end= "")
+    for k in range(n - i):
+        if i == 0 or k == 0 or k == n - i - 1 :
+            print("* ", end = "")
+        else:
+            print("  ", end = "")
+    print()
+
+print()
