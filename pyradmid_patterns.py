d89cf69029a59135475907ac2e50fbe191c9891f
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-08-23 21:08:16 +0530

    practicing questions

--- pyradmid_patterns.py
+++ pyradmid_patterns.py
@@ -104,6 +104,22 @@ for i in range(n):
             print("  ",end="")
     print()
 
+print()
+
+
+
+n = 5
+for i in range(n):
+    for j in range(n - i - 1):
+         print(" ", end = "")
+    for k in range(i + 1):
+        if k == 0 or k == i or i == n -1:
+            print("* ",end="")
+        else:
+            print("  ", end = "")
+    print()
+
+
 
 
 
