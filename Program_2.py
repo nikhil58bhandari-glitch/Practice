6f89c9a2cf9c996114b412e5ca858b0c458cd03e
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-08-25 00:03:31 +0530

    Practice Questions

--- Program_2.py
+++ Program_2.py
@@ -1,6 +1,10 @@
- # Power of 2 using anonymous function
+        # Q1-> Power of 2 using anonymous function-:
 
-power = lambda n: 2 ** n
+# power = lambda n: 2 ** n
+# n = int(input("Enter the power: "))
+# print(power(n))
 
-n = int(input("Enter the power: "))
-print(power(n))
+num = int(input("Enter the Number : "))
+n = list(map(lambda x: 2 ** x, range(num + 1)))
+for i in range(num + 1):
+    print(f"2 power {i} is-: {n[i]}")
