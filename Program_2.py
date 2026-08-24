1c33e5cbe71d79a9ddf611d69fc780134c191276
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-08-25 00:23:09 +0530

    Practice Questions

--- Program_2.py
+++ Program_2.py
@@ -4,7 +4,15 @@
 # n = int(input("Enter the power: "))
 # print(power(n))
 
-num = int(input("Enter the Number : "))
-n = list(map(lambda x: 2 ** x, range(num + 1)))
-for i in range(num + 1):
-    print(f"2 power {i} is-: {n[i]}")
+# num = int(input("Enter the Number : "))
+# n = list(map(lambda x: 2 ** x, range(num + 1)))
+# for i in range(num + 1):
+#     print(f"2 power {i} is-: {n[i]}")
+
+
+    # Q2-> Find Numbers divisible by another number
+
+num = int(input("Enter Number : "))
+upto = int(input("Enter a number upto we have to check : "))
+Division = list(filter(lambda x : x % num == 0, list(range(1, upto + 1))))
+print(f"The list of numbers which is divisible by {num} are {Division}")
