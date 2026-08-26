2ff9d78f00216c355904538b2dd12f5e65581496
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-08-26 23:26:34 +0530

    List Questions

---
+++ List.py
@@ -0,0 +1,19 @@
+      # Q1-> Check if list is empty -:
+
+list1 = [21,3,42,44,65,2,1,22]
+list2 = []
+
+if len(list1) == 0:
+    print("list1 is empty")
+else:
+    print(f"list1 has {len(list1)} elements")
+
+if not list2:
+    print("list2 is empty")
+
+
+       # Q2 -> Get the Last Element of the list -:
+
+list1 = [43,4,3,6,8,7,6]
+print(list1[len(list1) - 1])
+print(list1 [- 1])
