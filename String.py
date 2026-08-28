5c94639c063d384b9e697a02041105d05b0d4d2d
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-08-29 00:32:32 +0530

    String Questions

--- String.py
+++ String.py
@@ -46,19 +46,48 @@
 
    # Q5-: count the number of each vowel-:
 
-words = input("Enter the String -: ").upper()
-A = E = I = O = U = 0
-for i in words:
-    if i == 'A':
-      A += 1
-    elif i == 'E':
-        E += 1
-    elif i == 'I':
-        I += 1
-    elif i == 'O':
-        O += 1
-    elif i == 'U':
-        U += 1
-    else:
-        pass
-print(f"A = {A} E = {E} I = {I} O = {O} U = {U}")
+# words = input("Enter the String -: ").upper()
+# A = E = I = O = U = 0
+# for i in words:
+#     if i == 'A':
+#       A += 1
+#     elif i == 'E':
+#         E += 1
+#     elif i == 'I':
+#         I += 1
+#     elif i == 'O':
+#         O += 1
+#     elif i == 'U':
+#         U += 1
+#     else:
+#         pass
+# print(f"A = {A} E = {E} I = {I} O = {O} U = {U}")
+
+
+  # Q6-> Get SubString of a string-:
+
+s = 'Ande Sai Preveen'
+s1 = s[4:8]
+s2 = s[5:]
+s3 = s[:8:-1]
+print('S1:', s1)
+print('S2:', s2)
+print('S3:', s3)
+
+
+   # Q7-> sort words with alphabetical order-:
+
+alphabets = ['hai','i','had','many', 'hopes', 'on', 'tec', 'compony', 'for', 'that',
+            'i', 'had', 'prepared', 'very', 'much']
+
+sorted_one = sorted(alphabets)
+print(sorted_one)
+
+alphabets = ['hai','i','had','many', 'hopes', 'on', 'tec', 'compony', 'for', 'that',
+            'i', 'had', 'prepared', 'very', 'much']
+for i in range(len(alphabets)):
+    for j in range(len(alphabets)-i-1):
+        if alphabets[j] > alphabets[j + 1]:
+            alphabets[j], alphabets[j + 1] =  alphabets[j + 1], alphabets[j]
+print(alphabets)
+
