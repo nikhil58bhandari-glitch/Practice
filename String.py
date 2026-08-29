4176c718a4c4bbb8ab7b614909b5d3a46ad78316
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-08-29 23:18:36 +0530

    String Questions

--- String.py
+++ String.py
@@ -66,28 +66,52 @@
 
   # Q6-> Get SubString of a string-:
 
-s = 'Ande Sai Preveen'
-s1 = s[4:8]
-s2 = s[5:]
-s3 = s[:8:-1]
-print('S1:', s1)
-print('S2:', s2)
-print('S3:', s3)
+# s = 'Ande Sai Preveen'
+# s1 = s[4:8]
+# s2 = s[5:]
+# s3 = s[:8:-1]
+# print('S1:', s1)
+# print('S2:', s2)
+# print('S3:', s3)
 
 
    # Q7-> sort words with alphabetical order-:
 
-alphabets = ['hai','i','had','many', 'hopes', 'on', 'tec', 'compony', 'for', 'that',
-            'i', 'had', 'prepared', 'very', 'much']
+# alphabets = ['hai','i','had','many', 'hopes', 'on', 'tec', 'compony', 'for', 'that',
+#             'i', 'had', 'prepared', 'very', 'much']
+#
+# sorted_one = sorted(alphabets)
+# print(sorted_one)
+#
+# alphabets = ['hai','i','had','many', 'hopes', 'on', 'tec', 'compony', 'for', 'that',
+#             'i', 'had', 'prepared', 'very', 'much']
+# for i in range(len(alphabets)):
+#     for j in range(len(alphabets)-i-1):
+#         if alphabets[j] > alphabets[j + 1]:
+#             alphabets[j], alphabets[j + 1] =  alphabets[j + 1], alphabets[j]
+# print(alphabets)
 
-sorted_one = sorted(alphabets)
-print(sorted_one)
 
-alphabets = ['hai','i','had','many', 'hopes', 'on', 'tec', 'compony', 'for', 'that',
-            'i', 'had', 'prepared', 'very', 'much']
-for i in range(len(alphabets)):
-    for j in range(len(alphabets)-i-1):
-        if alphabets[j] > alphabets[j + 1]:
-            alphabets[j], alphabets[j + 1] =  alphabets[j + 1], alphabets[j]
-print(alphabets)
+  # Q7->  Trim Whitespace from a sting -:
+
+string = "Hai sai praveen"
+trim = ''
+for i in string:
+    if i != ' ':
+        trim = trim + i
+print(trim)
+
+string = "            hai sai praveen"
+print(string.strip())
+print(string.rstrip())
+print(string.lstrip())
+
+
+
+   # Q8-> Convert bytes to string-:
+
+byte_code = b'Hai praveen'
+decode_code = byte_code.decode()
+print(type(byte_code))
+print(type(decode_code))
 
