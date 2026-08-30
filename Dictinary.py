76b0b52dadfe7c4f291fbd7284ca07bfd1a4845f
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-08-30 23:57:21 +0530

    Dictinary

--- Dictinary.py
+++ Dictinary.py
@@ -35,48 +35,65 @@
 
    # Q3-> Iterate over Dictionaries using for loop -:
 
-my_dict = {'A' : 'APPEND', 'B':'BASECLASS', 'C':'CASE SENDER', 'D':'DATE BASE', 'E':'EFFICIENT'}
-
-for item,value in my_dict.items():   # .items() → gives key + value
-    print(f"{item} for {value}")
-
-for item in  my_dict.keys():       # .keys() → gives only keys
-    print(item, end=' ')
-print()
-
-for value in my_dict.values():   #  .values() → gives only values
-    print(value, end=' ')
-
-print()
+# my_dict = {'A' : 'APPEND', 'B':'BASECLASS', 'C':'CASE SENDER', 'D':'DATE BASE', 'E':'EFFICIENT'}
+#
+# for item,value in my_dict.items():   # .items() → gives key + value
+#     print(f"{item} for {value}")
+#
+# for item in  my_dict.keys():       # .keys() → gives only keys
+#     print(item, end=' ')
+# print()
+#
+# for value in my_dict.values():   #  .values() → gives only values
+#     print(value, end=' ')
+#
+# print()
 
 
     # Q4-> Sort  a dictionaty by value -:
 
-my_dict = {2:'pineapple', 3 : 'banana', 4: 'orange', 11 :'papaya'}
+# my_dict = {2:'pineapple', 3 : 'banana', 4: 'orange', 11 :'papaya'}
+#
+# # sorted_dict = dict(sorted(my_dict.items(), key = lambda x:x[1]))
+# sorted_dict = dict(
+#     sorted(              # sorted() needs to know what it should sort by
+#         my_dict.items(),
+#         key=lambda x: x[1]    #  lambda x: x[1]-> Take one item and return its value.
+#     )                                      #  What does key= mean?-> "When sorting, use the second element of each item."
+# )
+# print(sorted_dict)
+#
+# # print(dict(sorted(my_dict.items(), key = lambda x : x [1], reverse = True)))
+# print(
+#     dict(
+#         sorted(
+#             my_dict.items(),
+#             key=lambda x: x[1],
+#             reverse=True   # Sort in the opposite direction.
+#         )
+#     )
+# )
+# will_sorted = list(my_dict.items())
+# will_sorted.sort(key = lambda x : x[1])
+# print({key:value for key, value in will_sorted})
 
-# sorted_dict = dict(sorted(my_dict.items(), key = lambda x:x[1]))
-sorted_dict = dict(
-    sorted(              # sorted() needs to know what it should sort by
-        my_dict.items(),
-        key=lambda x: x[1]    #  lambda x: x[1]-> Take one item and return its value.
-    )                                      #  What does key= mean?-> "When sorting, use the second element of each item."
-)
-print(sorted_dict)
 
-# print(dict(sorted(my_dict.items(), key = lambda x : x [1], reverse = True)))
-print(
-    dict(
-        sorted(
-            my_dict.items(),
-            key=lambda x: x[1],
-            reverse=True   # Sort in the opposite direction.
-        )
-    )
-)
-will_sorted = list(my_dict.items())
-will_sorted.sort(key = lambda x : x[1])
-print({key:value for key, value in will_sorted})
+    # Q5-> Check if a key is present in a dictionary ->
 
+my_dict = {'A' : 'APPEND', 'B':'BASECLASS', 'C':'CASE SENDER', 'D':'DATE BASE', 'E':'EFFICIENT'}
+
+ele = 'N'
+
+if  ele in my_dict.keys():
+    print(f'{ele} key exist with {my_dict[ele]}')
+else:
+    print(f'{ele} not found')
+
+ele = 'C'
+if my_dict.get(ele) is not None:
+    print(f"{ele} key is present in dictionary with {my_dict.get(ele)}")
+else:
+    print(f'{ele} key is not in dictionary')
 
 
 
