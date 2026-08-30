1b3dcf7e4799e969f0984ab9219c195149a44317
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-08-30 23:47:57 +0530

    Dictinary

--- Dictinary.py
+++ Dictinary.py
@@ -1,37 +1,83 @@
     # Q1-> Created nested dictionary-:
 
-details = {
-    'name' : 'nikhil bhandari',
-    'age' : 22,
-    'qualifications' : 'B-Tech',
-    'Branch' : 'CSE',
-    'Address' : {
-        'door no' : 110,
-        'street' : 'nagraja mahoola',
-        'city' : 'srinagar garhwal',
-        'state' : 'uttrakhand',
-    }
-}
-print(details)
+# details = {
+#     'name' : 'nikhil bhandari',
+#     'age' : 22,
+#     'qualifications' : 'B-Tech',
+#     'Branch' : 'CSE',
+#     'Address' : {
+#         'door no' : 110,
+#         'street' : 'nagraja mahoola',
+#         'city' : 'srinagar garhwal',
+#         'state' : 'uttrakhand',
+#     }
+# }
+# print(details)
 
     # Q2-> Convert two lists into a dictionary-:
 
-list1 = [1,2,3,4,5,6]
-list2 = [1,4,9,16,2,15,36]
+# list1 = [1,2,3,4,5,6]
+# list2 = [1,4,9,16,2,15,36]
+#
+# my_dict = {}
+# for index, value in enumerate(list1):
+#
+#    # -> enumerate() gives you both index and value:
+#    my_dict[value] = list2[index]
+#
+# my_dict2 = {k:v for k, v in zip(list1,list2)} # k -> key, v -> value
+#
+# my_dict3 = dict(zip(list1, list2))
+#
+# print(my_dict)
+# print(my_dict2)
+# print(my_dict3)
 
-my_dict = {}
-for index, value in enumerate(list1):
+   # Q3-> Iterate over Dictionaries using for loop -:
 
-   # -> enumerate() gives you both index and value:
-   my_dict[value] = list2[index]
+my_dict = {'A' : 'APPEND', 'B':'BASECLASS', 'C':'CASE SENDER', 'D':'DATE BASE', 'E':'EFFICIENT'}
 
-my_dict2 = {k:v for k, v in zip(list1,list2)} # k -> key, v -> value 
+for item,value in my_dict.items():   # .items() → gives key + value
+    print(f"{item} for {value}")
 
-my_dict3 = dict(zip(list1, list2))
+for item in  my_dict.keys():       # .keys() → gives only keys
+    print(item, end=' ')
+print()
 
-print(my_dict)
-print(my_dict2)
-print(my_dict3)
+for value in my_dict.values():   #  .values() → gives only values
+    print(value, end=' ')
+
+print()
+
+
+    # Q4-> Sort  a dictionaty by value -:
+
+my_dict = {2:'pineapple', 3 : 'banana', 4: 'orange', 11 :'papaya'}
+
+# sorted_dict = dict(sorted(my_dict.items(), key = lambda x:x[1]))
+sorted_dict = dict(
+    sorted(              # sorted() needs to know what it should sort by
+        my_dict.items(),
+        key=lambda x: x[1]    #  lambda x: x[1]-> Take one item and return its value.
+    )                                      #  What does key= mean?-> "When sorting, use the second element of each item."
+)
+print(sorted_dict)
+
+# print(dict(sorted(my_dict.items(), key = lambda x : x [1], reverse = True)))
+print(
+    dict(
+        sorted(
+            my_dict.items(),
+            key=lambda x: x[1],
+            reverse=True   # Sort in the opposite direction.
+        )
+    )
+)
+will_sorted = list(my_dict.items())
+will_sorted.sort(key = lambda x : x[1])
+print({key:value for key, value in will_sorted})
+
+
 
 
 
