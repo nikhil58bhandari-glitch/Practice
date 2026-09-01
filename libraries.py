ceec9918042acd3fb0783cd4e95e3bec32873ef7
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-09-01 23:18:46 +0530

    Array in numpy

--- libraries.py
+++ libraries.py
@@ -15,17 +15,36 @@ print(type(a))
 x = [1,2,3,4,5]
 y = np.array(x)
 print(y)
-
-l = []
-for i in range(1,5):
-    int_1 = int(input('enter-: '))
-    l.append(int_1)
-print(l)
+#
+# l = []
+# for i in range(1,5):
+#     int_1 = int(input('enter-: '))
+#     l.append(int_1)
+# print(l)
 
 
    # ** Types of Array->
 
+ # ONE Dimensional -:
+y = np.array([1,2,3,4,5,6,5])
+print(y)
+print(y.ndim)  # .ndim tell the dimensional of array
 
+  # TWO Dimensional -:
+arr2 = np.array([[1,2,3,4],[5,6,7,8]])
+print(arr2)
+print(arr2.ndim)
+
+  # THREE Dimensional -:
+arr3 = np.array([[[1,2,3],[4,5,6],[7,8,9]]])
+print(arr3)
+print(arr3.ndim)
+
+
+  # N- Dimensional -:
+arrn = np.array([1,2,3],ndmin = 10)
+print(arrn)
+print(arrn.ndim)
 
 
 
