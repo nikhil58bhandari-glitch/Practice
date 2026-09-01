031ac42531b6b7ab8553c58a72b291e082a7b251
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-09-01 23:06:55 +0530

    Array in numpy

---
+++ libraries.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+print(np.__version__)
+y = [1,2,3,4,5]
+print(y)
+print(type(y))
+
+
+   # Array->
+
+a = np.array([1,2,3,4,5])
+print(a)
+print(type(a))
+
+x = [1,2,3,4,5]
+y = np.array(x)
+print(y)
+
+l = []
+for i in range(1,5):
+    int_1 = int(input('enter-: '))
+    l.append(int_1)
+print(l)
+
+
+   # ** Types of Array->
+
+
+
+
+
