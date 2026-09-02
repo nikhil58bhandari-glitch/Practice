0c203585c07dda8cff0968f25bee071f9a2e5af2
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-09-02 12:39:44 +0530

    Arithmetic Operations, Arithmetic Functions, and Indexing $ Slicing in Numpy

--- libraries_2.py
+++ libraries_2.py
@@ -76,18 +76,50 @@ import numpy as np
 
 
   # Arithmetic Functions ->
-var = np.array([1,2,3,4,5,6])
-print('min : ',np.min(var))
-print('max : ',np.max(var))
-print('arg max : ',np.argmax(var))
-print('arg min : ',np.argmin(var))
-print(np.sqrt(var))
-print(np.sin(var))
-print(np.cos(var))
-print(np.cumsum(var))
+# var = np.array([1,2,3,4,5,6])
+# print('min : ',np.min(var))
+# print('max : ',np.max(var))
+# print('arg max : ',np.argmax(var))
+# print('arg min : ',np.argmin(var))
+# print(np.sqrt(var))
+# print(np.sin(var))
+# print(np.cos(var))
+# print(np.cumsum(var))
+#
+# var1 =np.array([[1,2,3,4,5],[4,5,3,2,6]])
+# print(np.min(var1,axis = 0))
 
-var1 =np.array([[1,2,3,4,5],[4,5,3,2,6]])
-print(np.min(var1,axis = 0))
+
+    # * Indexing & Slicing in Numpy Array->
+
+var = np.array([1,2,3,4,5])
+print(var[1])
+print(var[-1])
+
+var1 = np.array([[1,2,3,4],[5,4,3,2]])
+print(var1)
+print(var1.ndim)
+print(var1[0,1])
+
+var2 = np.array([[[1,2,3],[4,5,6],[7,8,9]]])
+print(var2)
+print(var2.ndim)
+print(var2[0,1,2])
+print()
+
+  # Slicing->
+var = np.array([1,2,3,4,5,6])
+print(var)
+print()
+print('2 to 5 : ', var[1:5])
+print('2 to ending : ', var[1:])
+print('start to 5 :',var[:5])
+print('stop : ', var[1:5:2])
+print('stop : ', var[::2])
+
+var = np.array([[1,2,3,4],[2,4,5,6],[5,6,7,4]])
+print(var)
+print(var[1,1:])
 
 
 
